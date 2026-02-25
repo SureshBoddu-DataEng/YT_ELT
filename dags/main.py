c5c12d6c5dcb4bf23f1b93d6f4a483a95a04fd42
sureshboddu <sureshsurya595@example.com>
2026-02-24 18:40:26 -0600

    issue fix

--- dags/main.py
+++ dags/main.py
@@ -3,10 +3,10 @@ import pendulum
 from datetime import datetime, timedelta
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
-from api.video_stats import (
+from api.videostats import (
     get_playlist_id,
     get_video_ids,
-    extract_video_data,
+    get_video_data,
     save_to_json,
 )
 
@@ -47,7 +47,7 @@ with DAG(
     # Define tasks
     playlist_id = get_playlist_id()
     video_ids = get_video_ids(playlist_id)
-    extract_data = extract_video_data(video_ids)
+    extract_data = get_video_data(video_ids)
     save_to_json_task = save_to_json(extract_data)
 
     trigger_update_db = TriggerDagRunOperator(
